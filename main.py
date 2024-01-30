c59edb43db2d4eae8efd494af8248ffeca0729eb
ivanm <ivanm@pop-os.localdomain>
2024-01-30 11:55:49 +0200

    Added: basic geometry

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from tkinter import ttk
 
 gh = tk.Tk()
 gh.geometry('480x320')
-gh.attributes('-fullscreen', True)
+# gh.attributes('-fullscreen', True)
 canvas = tk.Canvas(gh, width=480, height=320)
 canvas.create_rectangle(0,0, 480, 320, fill='grey')
-canvas.create_rectangle(0,0, 480, 32, fill='white')
+canvas.create_rectangle(0,0, 480, 25, fill='white')
 
-canvas.create_text(115, 18, text='Reactor cores State:', fill='black', font='Helvetica 8 bold')
+canvas.create_text(70, 13, text='Reactor cores State:', fill='black', font='Helvetica 9 bold')
 canvas.pack()
 
 gh.mainloop()
