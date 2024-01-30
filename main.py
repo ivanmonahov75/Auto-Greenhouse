1750b9e4ce77f4d6a6c7acc3c4278e7bb479c933
ivanm <ivanm@pop-os.localdomain>
2024-01-30 11:32:05 +0200

    First commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
 gh = tk.Tk()
-# gh.attributes('-fullscreen', True)
+gh.attributes('-fullscreen', True)
 canvas = tk.Canvas(gh, width=480, height=320)
-canvas.create_rectangle(0,0, 480, 320, fill='light grey')
+canvas.create_rectangle(0,0, 480, 320, fill='grey')
 canvas.pack()
 gh.mainloop()
